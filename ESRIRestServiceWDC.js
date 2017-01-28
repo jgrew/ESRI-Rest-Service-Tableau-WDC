447b053977c4967c25285e087c6b9a837c103b7b
jgrew <jgrew8855@gmail.com>
2017-01-28 00:01:17 -0800

    update

--- ESRIRestServiceWDC.js
+++ ESRIRestServiceWDC.js
@@ -1,25 +1,34 @@
 (function() {
 	var myConnector = tableau.makeConnector()
 
+	var ESRITableauDataTypeMap = {
+        esriFieldTypeString:  tableau.dataTypeEnum.string,
+        esriFieldTypeDouble:  tableau.dataTypeEnum.float,
+        esriFieldTypeOID:  tableau.dataTypeEnum.int,
+        esriFieldTypeGlobalID:  tableau.dataTypeEnum.int,
+        esriFieldTypeSmallInteger:  tableau.dataTypeEnum.int,
+        esriFieldTypeDate:  tableau.dataTypeEnum.datetime
+    }
+
 	myConnector.getSchema = function(schemaCallback) {
-		tableau.log("Hello WDC!");
+		//tableau.log("Hello WDC!");
 
 		$.getJSON(tableau.connectionData + '?f=pjson', function(resp) {
-			tableau.log(resp);
+			//tableau.log(resp);
 
 			tableau.connectionName = tableau.connectionName + ': ' + resp.name
 
 			var fields = resp.fields;
 
-			feature_cols = fields.map(function (currentObject) {
+			var feature_cols = fields.map(function (currentObject) {
 				return {
 					id: currentObject.name,
 					alias: currentObject.alias,
-					dataType: currentObject.type
+					dataType: ESRITableauDataTypeMap[currentObject.type]
 				};
 			});
 
-			tableau.log(feature_cols);
+			//tableau.log(feature_cols);
 
 			var feature_info = {
 				id : resp.name,
@@ -27,31 +36,28 @@
 				columns : feature_cols
 			};
 
+			//tableau.log(feature_info);
+
+			schemaCallback([feature_info]);
 		})
-
-
 	};
 
 	myConnector.getData = function(table, doneCallback) {
+		$.getJSON(tableau.connectionData + '/query?where=OBJECTID>3D1&outFields=*&f=pjson', function(resp) {
+			tableau.log(resp);
 
+			var features = resp.features,
+				tableData = [];
+
+			tableData = features.map(function (currentObject){
+				return
+			});
+
+		})
+
+		//table.appendRows(tableData);
+		doneCallback();
 	};
-
-	convertDataType = function(typeESRI) {
-		switch (typeESRI) {
-			case 'esriFieldTypeOID':
-				type = tableau.dataTypeEnum.float;
-				break;
-			case 'esriFieldTypeString':
-				type = tableau.dataTypeEnum.string;
-				break;
-			case 'esriFieldTypeDouble':
-				type = tableau.dataTypeEnum.float;
-				break;
-			case 'esriFieldTypeDate':
-				type = tableau.dataTypeEnum.date;
-				break;
-		}
-	}
 
 	setupConnector = function() {
          var restServiceUrl = $('#restServiceUrl').val().trim();
